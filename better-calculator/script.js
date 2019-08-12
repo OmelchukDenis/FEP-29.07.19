c8ce82364466c0b0a55e7c741cbe3f7ae4b5991e
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2019-08-12 17:28:03 +0300

    Better calculator ready

---
+++ better-calculator/script.js
@@ -0,0 +1,40 @@
+let operation = null;
+let operand1 = null;
+let operand2 = null;
+let result = null;
+
+function add(a, b) { return a + b }
+function sub(a, b) { return a - b }
+function div(a, b) { return a / b }
+function mult(a, b) { return a * b }
+function operand(operandIndex){
+    let operand;
+
+    do {
+        operand = +prompt('Operand ' + operandIndex, '2');
+    } while (isNaN(operand));
+
+    return operand;
+}
+
+do {
+    operation = prompt('Choose action (add, sub, div, mult)', 'add');
+} while (
+    operation != 'add' &&
+    operation != 'sub' &&
+    operation != 'div' &&
+    operation != 'mult'
+);
+
+operand1 = getOperand(1);
+operand2 = getOperand(2);
+
+switch (operation) {
+    case 'add': result = add(operand1, operand2); break;
+    case 'sub': result = sub(operand1, operand2); break;
+    case 'div': result = div(operand1, operand2); break;
+    case 'mult': result = mult(operand1, operand2); break;
+}
+
+alert('Result: ' + result);
+
--- script.js
+++ script.js
@@ -1,47 +1,8 @@
-"use strict"
-//1   Спрашиваем оператор
-let operator;
-let operandsCount;
-let operand;
-let result = null;
+function abc(){
+        // console.log(b);
+        let b = 'abc'
 
-do {
-    operator = prompt('Choose operator', 'add');
-} while( operator != 'add' &&
-         operator != 'sub' &&
-         operator != 'div' &&
-         operator != 'mult'
-        )
-
-// 2  Сколько операндов
-do {
-    operandsCount = +prompt('Operands count', '2');
-} while( isNaN(operandsCount) ||
-        operandsCount <= 0 ||
-        operandsCount >= 5
-        )
-
-for (let i=1; i <= operandsCount; i++){
-    do {
-        operand = +prompt('Operand ' + i, '0');
-    } while(isNaN(operand))
-
-    if (result === null) {
-        result = operand;
-        continue;
-    }
-
-    switch (operator){
-        case 'add': result += operand; break;
-        case 'sub': result -= operand; break;
-        case 'div': result /= operand; break;
-        case 'mult': result *= operand; break;
-        default: console.warn(operator + 'is not valid');
-    }
-
-    console.log('Operand ' + i, operand);
+    console.log(b);
 }
 
-alert('Result: ' + result)
-
-console.log('operator', operator, operandsCount, result);
+abc()
